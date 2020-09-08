841570bbd77342c7b9bb4bceda9893dc29636b38
Hemanth <hemanth@orchids.edu.in>
2020-09-08 12:57:38 +0530

    Resolved conflicting npm modules 💥

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,18 +1,17 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-    },
-    extends: [
-      'plugin:react/recommended',
-      'airbnb',
-    ],
-    plugins: [
-      'react',
-      'react-hooks'
-    ],
-    rules: {
-      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-    },
-  };
-  
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  plugins: [
+    'react',
+    'react-hooks',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
