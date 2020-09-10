24ef182e682b09bed388625b52a08f5356505862
Hemanth <hemanth@orchids.edu.in>
2020-09-11 00:58:01 +0530

    Updates

---
+++ precache-manifest.3987224d218bdd6d3a57dfd3b663ac57.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5a489bcfd0c6dbfbcc5b6ad4f140c1",
+    "url": "/hemanththyagaraj/outfit.git/index.html"
+  },
+  {
+    "revision": "14361b28e735f86c966c",
+    "url": "/hemanththyagaraj/outfit.git/static/css/3.cae7141f.chunk.css"
+  },
+  {
+    "revision": "6bafb25d4ceb815b7363",
+    "url": "/hemanththyagaraj/outfit.git/static/css/4.496dd01c.chunk.css"
+  },
+  {
+    "revision": "e32d4a27755757805d54",
+    "url": "/hemanththyagaraj/outfit.git/static/css/5.0202de00.chunk.css"
+  },
+  {
+    "revision": "db5ab2715dde0ef6d6e1",
+    "url": "/hemanththyagaraj/outfit.git/static/css/main.0da45afc.chunk.css"
+  },
+  {
+    "revision": "b92b4a0dbe9fed7d0026",
+    "url": "/hemanththyagaraj/outfit.git/static/js/2.46629c13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hemanththyagaraj/outfit.git/static/js/2.46629c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14361b28e735f86c966c",
+    "url": "/hemanththyagaraj/outfit.git/static/js/3.b2017467.chunk.js"
+  },
+  {
+    "revision": "6bafb25d4ceb815b7363",
+    "url": "/hemanththyagaraj/outfit.git/static/js/4.d24da2f9.chunk.js"
+  },
+  {
+    "revision": "e32d4a27755757805d54",
+    "url": "/hemanththyagaraj/outfit.git/static/js/5.08b88196.chunk.js"
+  },
+  {
+    "revision": "9457aaa42effe60bffbc",
+    "url": "/hemanththyagaraj/outfit.git/static/js/6.05df412b.chunk.js"
+  },
+  {
+    "revision": "db5ab2715dde0ef6d6e1",
+    "url": "/hemanththyagaraj/outfit.git/static/js/main.038b4111.chunk.js"
+  },
+  {
+    "revision": "500ee98dcf20b089e981",
+    "url": "/hemanththyagaraj/outfit.git/static/js/runtime-main.59b2cc1a.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/hemanththyagaraj/outfit.git/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/hemanththyagaraj/outfit.git/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/hemanththyagaraj/outfit.git/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "bdfc0ca40b3c9e3c375b8ea7a9ccc6ce",
+    "url": "/hemanththyagaraj/outfit.git/static/media/github.bdfc0ca4.svg"
+  },
+  {
+    "revision": "ae99141a181f413a1cdd10093aa79a90",
+    "url": "/hemanththyagaraj/outfit.git/static/media/instagram.ae99141a.svg"
+  },
+  {
+    "revision": "369f2ff8d1b4ac66e0f4799d8e0ec0b6",
+    "url": "/hemanththyagaraj/outfit.git/static/media/kid.369f2ff8.jpg"
+  },
+  {
+    "revision": "07b884b2960803fd7c706779d4f0a649",
+    "url": "/hemanththyagaraj/outfit.git/static/media/logo.07b884b2.svg"
+  },
+  {
+    "revision": "f8348b53f5edd3318472c10d8de34b0e",
+    "url": "/hemanththyagaraj/outfit.git/static/media/logo__light.f8348b53.svg"
+  },
+  {
+    "revision": "80da7ced5975b5c3e0965519f9750f4a",
+    "url": "/hemanththyagaraj/outfit.git/static/media/men.80da7ced.jpg"
+  },
+  {
+    "revision": "bc05d7cc222e6cda115964b2d3c458e7",
+    "url": "/hemanththyagaraj/outfit.git/static/media/model.bc05d7cc.png"
+  },
+  {
+    "revision": "bb9f600961744fd609c8e131701c18aa",
+    "url": "/hemanththyagaraj/outfit.git/static/media/twitter.bb9f6009.svg"
+  },
+  {
+    "revision": "31ebbac8b0fe255870398534bcebd9de",
+    "url": "/hemanththyagaraj/outfit.git/static/media/women.31ebbac8.jpg"
+  }
+]);
---
+++ static/js/6.05df412b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[6],{54:function(t,e,n){"use strict";n.r(e);var i=n(0),s=n.n(i);e.default=function(){return s.a.createElement("div",null,"this is the about us page")}}}]);
+//# sourceMappingURL=6.05df412b.chunk.js.map
